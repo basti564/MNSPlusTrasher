915cd2f647aae5115ae0c01d887823099621aeb8
basti564 <34898868+BastianOliverSchwickert@users.noreply.github.com>
2020-01-26 00:06:05 +0100

    Update 1.9.6

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.9.5")>
-<Assembly: AssemblyFileVersion("1.9.5")>
+<Assembly: AssemblyVersion("1.9.6")>
+<Assembly: AssemblyFileVersion("1.9.6")>
 <Assembly: NeutralResourcesLanguage("en")>
